fd8d3fd4aa8abf8047b84c98da15734d798d1dc2
Evy32 <evbp080116@gmail.com>
2023-06-28 19:53:58 -0500

    Version 1.0.0.0

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3001;
 app.use(express.static(path.resolve(__dirname,'public')));
 
 app.get('/', (req, res)=>{
@@ -14,6 +15,5 @@ app.get('/register', (req, res)=>{
 app.get('/login', (req, res)=>{
     res.sendFile(path.resolve(__dirname, './views/login.html'))
 })
-app.listen(3000, ()=>{
-    console.log("Servidor corriendo en el puerto 3000");
-})
+
+app.listen(port, ()=>console.log(`Servidor corriendo en: http://localhotst:${port}`));
